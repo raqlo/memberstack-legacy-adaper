27045a0f62d8fb0ad542b773d3db2799339f6790
Raquel Lopez <raquelcle@gmail.com>
2025-06-17 16:50:23 -0400

    Add: Define `MemberstackV1` type and update global `MemberStack` typings
    Introduce `MemberstackV1` type to provide detailed typings for Memberstack v1 methods and member API. Update global `MemberStack` declaration to use `Partial<MemberstackV1>`.

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -6,12 +6,20 @@ import type {
 import type {AdapterConfig} from "@/config";
 
 export type MemberstackDom = ReturnType<typeof MsTypes.init>;
+export type MemberstackV1 = {
+    getMember: () => any;
+    onReady: Promise<unknown>;
+    getToken: () => any;
+    reload: () => any;
+    logout: () => any;
+    selectMembership: () => any;
+    __resolveOnReady?: (val: any) => void;
+}
 
 declare global {
     interface Window {
         Webflow: any;
-        MemberStack: any; // ToDo create v1 lib type and type this declaration type
-        __MemberStackOriginal: typeof window.MemberStack;
+        MemberStack: Partial<MemberstackV1>
         memberstackConfig: AdapterConfig
         $memberstackReady?: boolean;
         $memberstackDom?: MemberstackDom;
